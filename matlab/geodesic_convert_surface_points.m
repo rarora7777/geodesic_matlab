814d9c32c89c495ca54ba5a7052b06deb655df8d
Rahul Arora <arorar@cs.toronto.edu>
2020-05-01 23:30:41 -0400

    Edited matlab scripts for implicit points

--- matlab/geodesic_convert_surface_points.m
+++ matlab/geodesic_convert_surface_points.m
@@ -2,44 +2,66 @@
 %conversion between C++ and matlab surface point representation
 % Danil Kirsanov, 09/2007 
 
-function q = geodesic_convert_surface_points(p)
+function q = geodesic_convert_surface_points(p, implicit_io)
 
 if isempty(p)
     q = [];
     return;
 end
 
+if nargin < 2
+    implicit_io = false;
+end
+
 point_types = {'vertex', 'edge', 'face'};
 
-if ~isa(p,'numeric')       %convert from matlab to C++
-    num_points = length(p);
-    q = zeros(num_points*5, 1);
-    for i = 1:num_points
-        shift = (i-1)*5;
-        q(shift + 1) = p{i}.x;
-        q(shift + 2) = p{i}.y;
-        q(shift + 3) = p{i}.z;
-        q(shift + 5) = p{i}.id - 1;
-        
-        type = find(strcmp(point_types, p{i}.type));
-        if isempty(type)
-            disp('error: point type is incorrect');
-            q = [];
-            return;
-        else
-            q(shift + 4) = type - 1;
+if ~implicit_io
+    if ~isa(p,'numeric')       %convert from matlab to C++
+        num_points = length(p);
+        q = zeros(num_points*5, 1);
+        for i = 1:num_points
+            shift = (i-1)*5;
+            q(shift + 1) = p{i}.x;
+            q(shift + 2) = p{i}.y;
+            q(shift + 3) = p{i}.z;
+            q(shift + 5) = p{i}.id - 1;
+
+            type = find(strcmp(point_types, p{i}.type));
+            if isempty(type)
+                disp('error: point type is incorrect');
+                q = [];
+                return;
+            else
+                q(shift + 4) = type - 1;
+            end
+        end
+    else                    %convert from C++ to matlab
+        num_points = length(p)/5;
+        q = cell(num_points, 1);
+        for i = 1:num_points
+            shift = (i-1)*5;
+            point.x = p(shift + 1);
+            point.y = p(shift + 2);
+            point.z = p(shift + 3);
+            point.type = point_types(p(shift + 4) + 1);
+            point.id = p(shift + 5) + 1;
+
+            q{i} = point;
         end
     end
-else                    %convert from C++ to matlab
-    num_points = length(p)/5;
+else
+    % convert from C++ to matlab only for now
+    num_points = length(p)/7;
     q = cell(num_points, 1);
     for i = 1:num_points
-        shift = (i-1)*5;
+        shift = (i-1)*7;
         point.x = p(shift + 1);
         point.y = p(shift + 2);
         point.z = p(shift + 3);
         point.type = point_types(p(shift + 4) + 1);
         point.id = p(shift + 5) + 1;
+        point.b0 = p(shift + 6);
+        point.b1 = p(shift + 7);
         
         q{i} = point;
     end
--- matlab/geodesic_trace_back.m
+++ matlab/geodesic_trace_back.m
@@ -1,4 +1,4 @@
-function path = geodesic_trace_back(algorithm, destination)
+function path = geodesic_trace_back(algorithm, destination, implicit_output)
 
 global geodesic_library;
 
@@ -6,7 +6,20 @@ tmp{1} = destination;
 d = geodesic_convert_surface_points(tmp);
 
 tmp = libpointer('doublePtrPtr');
-[path_length, ~, path_double] = calllib(geodesic_library, 'trace_back', algorithm.id, d, tmp);
 
-setdatatype(path_double, 'doublePtr', path_length*5);
-path = geodesic_convert_surface_points(path_double.Value);
+if nargin < 3
+    implicit_output = false;
+end
+
+if ~implicit_output
+    [path_length, ~, path_double] = ...
+        calllib(geodesic_library, 'trace_back', algorithm.id, d, tmp);
+    setdatatype(path_double, 'doublePtr', path_length*5);
+    path = geodesic_convert_surface_points(path_double.Value);
+else
+    [path_length, ~, path_double] = ...
+        calllib(geodesic_library, 'trace_back_implicit', algorithm.id, d, tmp);
+    setdatatype(path_double, 'doublePtr', path_length*7);
+    path = geodesic_convert_surface_points(path_double.Value, true);
+end
+
